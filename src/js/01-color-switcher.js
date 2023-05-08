68330178ceb98e9d44931163516269e39da4da8a
MichalPoniatowski <m.poniatowski87@gmail.com>
2023-05-08 21:28:10 +0200

    task 1 corrected

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,8 @@
 const startBtnEl = document.querySelector('[data-start]');
 const stopBtnEl = document.querySelector('[data-stop]');
 
+let randomColorBg;
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
